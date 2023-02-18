1b465aea6fe1571434e1cd6845b41afe2876c666
sergibonell <80847729+sergibonell@users.noreply.github.com>
2023-02-18 17:51:17 +0100

    Added jump animation

--- Assets/AnimationManager.cs
+++ Assets/AnimationManager.cs
@@ -18,5 +18,10 @@ public class AnimationManager : MonoBehaviour
     {
         float speed = new Vector2(movementInterface.velocity.x, movementInterface.velocity.z).magnitude;
         animator.SetFloat("Speed", speed);
+
+        if (movementInterface.jumpedThisFrame)
+            animator.SetTrigger("Jumped");
+        if (movementInterface.landedThisFrame)
+            animator.SetTrigger("Landed");
     }
 }
