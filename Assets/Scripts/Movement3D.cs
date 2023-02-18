d76eaa4d4d1c731eed2b546e8b66e2d4240a9b69
sergibonell <80847729+sergibonell@users.noreply.github.com>
2023-02-18 15:42:16 +0100

    Imported personal movement script

---
+++ Assets/Scripts/Movement3D.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement3D : MonoBehaviour
+{
+    [Header("MOVEMENT")]
+    [SerializeField] float regularSpeed;
+    [SerializeField] float sprintSpeed;
+    float moveSpeed;
+    [Header("JUMPING")]
+    [SerializeField] float gravity;
+    [SerializeField] float jumpHeight;
+
+    Transform cameraTransform;
+    float currentFallSpeed;
+    Vector2 currentMoveSpeed;
+    Vector2 moveInput;
+
+    bool isMoving;
+    bool jumpedThisFrame;
+    bool isJumping;
+
+    CharacterController controller;
+    PlayerInput input;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        input = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+
+        moveSpeed = regularSpeed;
+    }
+
+    void OnEnable()
+    {
+        input.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        input.enabled = false;
+    }
+
+    void Start()
+    {
+        input.actions["Move"].started += _ => isMoving = true;
+        input.actions["Move"].performed += it => moveInput = it.ReadValue<Vector2>();
+        input.actions["Move"].canceled += _ => { isMoving = false; currentMoveSpeed = Vector2.zero; };
+
+        input.actions["Jump"].started += _ => jumpedThisFrame = CheckJump();
+
+        input.actions["Sprint"].started += _ => moveSpeed = sprintSpeed;
+        input.actions["Sprint"].canceled += _ => moveSpeed = regularSpeed;
+    }
+
+    void Update()
+    {
+        if (jumpedThisFrame)
+        {
+            currentFallSpeed = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            jumpedThisFrame = false;
+        }
+        else if(controller.isGrounded)
+        {
+            currentFallSpeed = -2;
+            isJumping = false;
+        }
+        else
+        {
+            currentFallSpeed = currentFallSpeed + gravity * Time.deltaTime;
+        }
+
+        if (isMoving)
+        {
+            currentMoveSpeed = InputToMoveSpeed(moveInput, cameraTransform);
+        }
+
+        controller.Move(new Vector3(currentMoveSpeed.x, currentFallSpeed, currentMoveSpeed.y) * Time.deltaTime);
+    }
+
+    // Takes input and transform of where we're looking at (camera) and turns it into relative move speed
+    Vector2 InputToMoveSpeed(Vector2 input, Transform lookTransform = null)
+    {
+        Vector2 move;
+        if(lookTransform == null)
+        {
+            move = input;
+        }
+        else
+        {
+            Vector3 dir = new Vector3(input.x, 0, input.y);
+            dir = Quaternion.Euler(0, lookTransform.rotation.eulerAngles.y, 0) * dir;
+            move = new Vector2(dir.x, dir.z);
+        }
+        return move * moveSpeed;
+    }
+
+    // Check if we can jump this frame
+    bool CheckJump()
+    {
+        bool jumpedThisFrame = false;
+        if (!isJumping && controller.isGrounded)
+        {
+            isJumping = true;
+            jumpedThisFrame = true;
+        }
+        return jumpedThisFrame;
+    }
+}
